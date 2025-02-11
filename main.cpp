187260f2f15937e7d56992f99755d0a3d639e39b
hhhssh <hanshermawan1@gmail.com>
2025-02-10 19:54:35 -0800

    Finished

--- main.cpp
+++ main.cpp
@@ -6,24 +6,32 @@ using namespace std;
 
 int main()
 {
-    int num1, num2, product
+  int num1, num2, product;
 
-        cout << "\nMy name is  "
-        "Replace this text with your full name" << endl;
+  cout << "\nMy name is Hans Hermawan" << endl;
 
-    cout << "\nEnter the first number >";
-    cin >> num2;
+  cout << "\nEnter the first number >";
+  cin >> num2;
 
-    cout << "\nEnter the second number  ";
-    cin >> num11;
+  cout << "\nEnter the second number  ";
+  cin >> num1;
 
-    product = num1 + num2;
-    cout << "\nThe sum of " << num1 << " and " << sum << " is " << num2 << endl;
+  product = num1 + num2;
+  cout << "\nThe sum of " << num1 << " and " << product << " is " << num2 << endl;
 
-    return 0;
+  return 0;
 }
 
 /*
   PASTE YOUR PROGRAM OUTPUT HERE
+  --------------------------------
+
+  My name is Hans Hermawan
+
+  Enter the first number >2
+
+  Enter the second number  2
+
+  The sum of 2 and 4 is 2
 */
 
